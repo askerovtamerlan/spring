9be5fc3c0fb8be89c977cebdb517e3700839005b
askerovtamerlan <tomcool@yandex.ru>
2024-01-09 04:57:13 +0300

    new version

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { texture, uniform } from 'three/nodes'
 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FlakesTexture } from 'three/addons/textures/FlakesTexture.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 // import { TextureLoader } from 'three';
 
@@ -18,12 +19,25 @@ camera.position.z = 3;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-const light = new THREE.AmbientLight(0xede485, 0.7);
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+// pmremGenerator.compileEquirectangularShader();
+
+// THREE.DefaultLoadingManager.onLoad = function () {
+
+//     pmremGenerator.dispose();
+
+// };
+
+const light = new THREE.AmbientLight(0xFFFFFF, 20);
 light.position.set(0, 0, 0);
 scene.add(light);
 
-const exposure = -12.5
-renderer.physicallyCorrectLight = true
+renderer.outputColorspace = THREE.SRGBColorSpace;
+
+const exposure = -2.7
+// const exposure = -2.5
+
+// renderer.physicallyCorrectLight = true
 
 // renderer.toneMapping = THREE.LinearToneMapping
 // renderer.toneMapping = THREE.ReinhardToneMapping
@@ -44,8 +58,7 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const pmremGenerator = new THREE.PMREMGenerator(renderer);
-pmremGenerator.compileEquirectangularShader();
+
 
 // animation clips 
 
@@ -57,147 +70,197 @@ let prevTime = 0
 const loader = new GLTFLoader()
     .setCrossOrigin('anonymous');
 
-new RGBELoader().load('quarry_01_1k.hdr', function (env) {
-    env.mapping = THREE.EquirectangularReflectionMapping;
-    scene.environment = env
-})
+let envmap = undefined
 
-const whiteMaterial = new THREE.MeshPhongMaterial({
-    // color: '#923434',
-    side: THREE.DoubleSide
-});
+new RGBELoader().load('spruit_sunrise_1k.hdr', function (env) {
+    // env.mapping = THREE.EquirectangularReflectionMapping;
+    // scene.environment = env
 
-const greenMaterial = new THREE.MeshLambertMaterial({
-    // color: '#923434',
-    side: THREE.DoubleSide
-})
+    envmap = pmremGenerator.fromCubemap(env)
 
-let EarthObj = new THREE.Object3D();
+    let EarthObj = new THREE.Object3D();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, whiteMaterial);
-// scene.add( cube );
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // const cube = new THREE.Mesh(geometry, whiteMaterial);
+    // scene.add( cube );
 
-const textureLoader = new THREE.TextureLoader()
-
-const diffuseMap = textureLoader.load('./pol.png')
-diffuseMap.colorSpace = THREE.SRGBColorSpace
-diffuseMap.wrapS = THREE.RepeatWrapping;
-
-const normalMap = textureLoader.load('./textures/bakenormalmap.png')
-normalMap.wrapS = THREE.RepeatWrapping;
-
-const mMap = textureLoader.load('./metallicRoughness.png')
-
-const rMap = textureLoader.load('./textures/bakerougnessmap.png')
-rMap.wrapS = THREE.RepeatWrapping;
-
-// const rmMapNode = texture(rmMap)
-
-function startAnimation(gltf) {
-    clips = gltf.animations
-
-    mixer = new THREE.AnimationMixer(gltf.scene)
-
-    clips.forEach((clip) => {
-        mixer.clipAction(clip).reset().play()
-    })
-}
-
-function centerModel(gltf) {
-    let model = gltf.scene
-
-    const box = new THREE.Box3().setFromObject(model);
-    const size = box.getSize(new THREE.Vector3()).length();
-    const center = box.getCenter(new THREE.Vector3());
-
-    model.position.x += model.position.x - center.x;
-    model.position.y += model.position.y - center.y;
-    model.position.z += model.position.z - center.z;
-}
-
-let object = new THREE.Object3D()
-
-loader.load('./block_v1-1.glb', function (gltf) {
-
-    let model = gltf.scene
-    let animations = gltf.animations
-
-    startAnimation(gltf)
-    // centerModel(gltf)
+    const textureLoader = new THREE.TextureLoader()
 
 
+    function createFlakesMaterial() {
 
+        let flakesTexture = new THREE.CanvasTexture(new FlakesTexture());
 
+        flakesTexture.wrapS = THREE.RepeatWrapping;
+        flakesTexture.wrapT = THREE.RepeatWrapping;
+        flakesTexture.repeat.x = 1.17;
+        flakesTexture.repeat.y = 0.9;
+        flakesTexture.anisotropy = 15;
 
-
-    console.log(gltf)
-
-    let a = 0.1
-
-    model.scale.set(1 * a, 1 * a, 1 * a)
-    model.rotation.set(0, Math.PI / 2, 0)
-
-    model.traverse(function (child) {
-        if (child instanceof THREE.PointLight && child != undefined) {
-            console.log(child.name)
-
-            // object.add(child)
-
-            // child.castShadow = true;
+        const flakesMatChars = {
+            side: THREE.DoubleSide,
+            clearcoat: 0.85,
+            clearcoatRoughness: 0.6,
+            metalness: 0,
+            roughness: 0.55,
+            normalMap: flakesTexture,
+            color: 0x443e24,
+            normalScale: new THREE.Vector2(0.5, 0.15),
+            envMap: envmap.texture
         }
 
-        if (child instanceof THREE.Mesh && child != undefined) {
-            // console.log(child);
-            // child.material.side = THREE.FrontSide;  
-            // console.log(child.name)
-            let barkPineMaterial = undefined
+        let flakesMaterial = new THREE.MeshPhysicalMaterial(flakesMatChars)
 
-            if (child.material.name == "bark pine") {
-                if (child) {
-
-                    // barkPineMaterial = new THREE.MeshPhongMaterial().copy(child.material)
-                    // child.material = whiteMaterial
-                    // console.log(child.name)
-                    // console.log(child)
-                    // EarthObj.add(child)
-                }
-            }
-
-            if (child.material.name == "Material.007") {
-                if (child) {
-
-                    child.material = greenMaterial
-                    child.material.map = diffuseMap
-                    // child.material.normalMap = normalMap
-
-                    // child.material.metalnessMap = mMap
-                    child.material.roughnessMap = rMap
-                    child.material.roughness = 0.2
-                    child.material.metalness = 0.1
+        return flakesMaterial
+    }
 
 
-                    // child.material.needsUpdate = true;
-                    console.log(child.name)
-                    console.log(child)
-                    // EarthObj.add(child)
-                    // object.add(child)
 
-                }
-            }
+    const diffuseMap = textureLoader.load('./pol.png')
+    diffuseMap.colorSpace = THREE.SRGBColorSpace
+    diffuseMap.wrapS = THREE.RepeatWrapping;
+
+    const normalMap = textureLoader.load('./textures/bakenormalmap.png')
+    normalMap.wrapS = THREE.RepeatWrapping;
+
+    const mMap = textureLoader.load('./metallicRoughness.png')
+
+    const rMap = textureLoader.load('./textures/bakerougnessmap.png')
+    rMap.wrapS = THREE.RepeatWrapping;
+
+    // const rmMapNode = texture(rmMap)
 
 
-            // console.log(child.name)  
-
-            // if (child.name === 'Earthdots') {
-            // child.material = whiteMaterial  
-            // }
-        }
+    const whiteMaterial = new THREE.MeshPhongMaterial({
+        // color: '#923434',
+        side: THREE.DoubleSide,
     });
 
-    console.log(object)
-    scene.add(model)
+    const greenMaterial = new THREE.MeshLambertMaterial()
+
+    const shaderMaterial = new THREE.ShaderMaterial({
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent,
+        side: THREE.DoubleSide,
+        // normalMap: flakesTexture
+    })
+
+    function startAnimation(gltf) {
+        clips = gltf.animations
+
+        mixer = new THREE.AnimationMixer(gltf.scene)
+
+        clips.forEach((clip) => {
+            mixer.clipAction(clip).reset().play()
+        })
+    }
+
+    function centerModel(gltf) {
+        let model = gltf.scene
+
+        const box = new THREE.Box3().setFromObject(model);
+        const size = box.getSize(new THREE.Vector3()).length();
+        const center = box.getCenter(new THREE.Vector3());
+
+        // model.position.x += model.position.x - center.x;
+        // model.position.y += model.position.y - center.y;
+        // model.position.z += model.position.z - center.z;
+
+        // camera.lookAt(center)
+        camera.updateProjectionMatrix()
+    }
+
+    let object = new THREE.Object3D()
+
+    loader.load('./block_v1-1.glb', function (gltf) {
+
+        let model = gltf.scene
+        let animations = gltf.animations
+
+        startAnimation(gltf)
+        centerModel(gltf)
+
+
+
+
+
+
+        console.log(gltf)
+
+        let a = 0.1
+
+        model.scale.set(1 * a, 1 * a, 1 * a)
+        model.rotation.set(0, Math.PI / 2, 0)
+
+        model.traverse(function (child) {
+            if (child instanceof THREE.PointLight && child != undefined) {
+                console.log(child.name)
+
+                console.log(child.intensity)
+                console.log(child.color)
+                child.color = new THREE.Color(0x6b6446)
+                child.intensity = 1.065
+
+                // child.castShadow = true
+
+                // object.add(child)
+
+                // child.castShadow = true;
+            }
+
+            if (child instanceof THREE.Mesh && child != undefined) {
+                // console.log(child);
+                // child.material.side = THREE.FrontSide;  
+                // console.log(child.name)
+                let barkPineMaterial = undefined
+
+                if (child.material.name == "bark pine") {
+                    if (child) {
+
+                        // barkPineMaterial = new THREE.MeshPhongMaterial().copy(child.material)
+                        // child.material = whiteMaterial
+                        // console.log(child.name)
+                        // console.log(child)
+                        // EarthObj.add(child)
+                    }
+                }
+
+                if (child.material.name == "Material.007") {
+                    if (child) {
+
+                        child.material = createFlakesMaterial()
+                        // child.material.map = diffuseMap
+                        // child.material.normalMap = normalMap
+
+                        // child.material.metalnessMap = mMap
+                        // child.material.roughnessMap = rMap
+                        // child.material.roughness = 
+                        // child.material.metalness = 1
+
+
+                        // child.material.needsUpdate = true;
+                        console.log(child.name)
+                        console.log(child)
+                        // EarthObj.add(child)
+                        // object.add(child)
+
+                    }
+                }
+
+
+                // console.log(child.name)  
+
+                // if (child.name === 'Earthdots') {
+                // child.material = whiteMaterial  
+                // }
+            }
+        });
+
+        console.log(object)
+        scene.add(model)
+
+    })
 
 })
 
